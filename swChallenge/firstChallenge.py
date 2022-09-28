4fb7cf20edb5582d0f21e523428a3c7343472084
rlysmorales <rlysmorales@gmail.com>
2022-09-28 21:42:25 +0000

    manipulating lists

---
+++ swChallenge/firstChallenge.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def main():
+    name = input("What is your name?")
+    day = input ("What day of the week it is?")
+
+    print("Hello, " +name + "! Happy " + day + " of the week!")
+
+main()
