fa5035988ff23d9167e6d3378daae0ec802239e9
rlysmorales <rlysmorales@gmail.com>
2022-09-29 20:09:23 +0000

    renaming and moving files

---
+++ moveplease01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# standard library imports
+import shutil
+import os
+
+def main():
+    
+    # move into this working directory
+    os.chdir('/home/student/mycode/')
+    # try moving the file raynor.obj into ceph_storage/ dir
+    shutil.move('raynor.obj', 'ceph_storage/')
+    # program will pause while we accept input
+    xname = input('What is the new name for kerrigan.obj? ')
+    shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
+
+main()
