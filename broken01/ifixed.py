948aa1f41e817cbddbed5b3c632e9115cea3b96b
rlysmorales <rlysmorales@gmail.com>
2022-09-30 16:57:14 +0000

    studying for logic

--- broken01/ifixed.py
+++ broken01/ifixed.py
@@ -8,7 +8,7 @@ operation = ""
 while (calc1 != "q"):
     print("\nWhat is the first operator? Or, enter q to quit: ")
     calc1 = input()
-    if calc1 == "Q":
+    if calc1 == "q":
         break
     calc1 = float(calc1)
     print("\nWhat is the second operator? Or, enter q to quit: ")
@@ -17,7 +17,7 @@ while (calc1 != "q"):
         break
     calc2 = float(calc2)
     print("Enter an operation to perform on the two operators (+ or -): ")
-    operation = raw_input()
+    operation = input()
     if operation == "+":
         print("\n" + str(calc1) + " + " + str(calc2) + " = " + str(calc1 + calc2))
     elif  operation == '-':
